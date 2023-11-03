e3eac7d650c87b6dc59bc89fa57296d4a19ae8b6
Felipe Sebben <felipesebben@yahoo.com.br>
2023-11-03 16:33:32 -0300

    Transform to Load ok

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
 from pipeline.extract import extract_from_excel
+from pipeline.transform import concatenate_data_frames
+from pipeline.load import load_excel
 
-dataframes_lists = extract_from_excel("data/input")
-print(dataframes_lists)
+if __name__ == "__main__":
+    dataframes_list = extract_from_excel("data/input")
+    data_frame = concatenate_data_frames(dataframes_list)
+    load_excel(data_frame, "data/output", "output")
---
+++ app/pipeline/load.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import os
+
+
+
+def load_excel(data_frame: pd.DataFrame, output_path: str, file_name: str) -> str:
+    """
+    Receive DataFrame and save it as an Excel file.
+
+    args:
+    data_frame (pd.DataFrame): dataframe to be exported as Excel file
+    output_path (str): path in which the file will be exported
+    file_name (str): name of file to be exported
+
+
+    return "File successfully saved"
+    """
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    data_frame.to_excel(f"{output_path}/{file_name}.xlsx", index=False)
+    return "File successfully saved"
